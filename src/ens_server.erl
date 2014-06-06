60373a47dd81c32c4a6bbedf79717b11cbcb9162
dongaihua <dongaihua@dongaihua.local>
2014-06-06 17:33:39 +0800

    monitor node down

--- src/ens_server.erl
+++ src/ens_server.erl
@@ -37,14 +37,22 @@ init(_Args) ->
 	create_process_table(),
 	create_node_table(),
 	NodeInfo = write_node_record(),
+	register_to(MasterNode1),
+	register_to(MasterNode2),
 	{ok, #state{node_info=NodeInfo}}.
 
 handle_call(_Msg, _From, State) ->
 	{reply, ok, State}.
 
+handle_cast({nodeup, Node}, State) ->
+	monitor_node(Node, true),	
+	{noreply, State};
 handle_cast(_Msg, State) ->
     {noreply, State}.
 
+handle_info({nodedown, Node}, State) ->
+	delete_node(Node),
+	{noreply, State};
 handle_info(_Info, State) ->
     {noreply, State}.
 
@@ -63,7 +71,7 @@ create_node_table() ->
 	create_table(ens_node, record_info(fields, ens_node)).
 
 create_process_table() ->
-	{ok,  NodeType} = application:get_env(node_type),
+	NodeType = application:get_env(ens, node_type, player),
 	Table = table_name(NodeType),
 	Info = [id, node, pid],
 	create_table(Table, Info).		
@@ -77,15 +85,27 @@ create_table(Table, Info) ->
 	end.
 	
 write_node_record() ->
-	{ok,  NodeType} = application:get_env(node_type),
+	NodeType = application:get_env(ens, node_type, master),
 	NodeInfo = #ens_node{type=NodeType, node=node()},
 	mnesia:dirty_write(NodeInfo),
-	NodeInfo.
+	NodeInfo.	
+
+get_node_type(Node) ->
+	Table = ens_node,
+	Record = {Table, '$1', Node},
+	case mnesia:dirty_select(Table, [{Record, [], ['$1']}]) of
+		[Type|_] ->
+			{ok, Type};
+		[] ->
+			{error, not_found};
+		{aborted, Reason} ->
+			{error, Reason}
+	end.	
 
 get(Type, Id) ->
 	Table = table_name(Type),
 	Record = {Table, Id, '_', '$1'},
-	case mnesia:dirty_select(Table, [{Record, [], ['$1']}]) of
+	case catch mnesia:dirty_select(Table, [{Record, [], ['$1']}]) of
 		[Pid|_] ->
 			{ok, Pid};
 		[] ->
@@ -102,14 +122,36 @@ delete(Type, Id) ->
 	Table = table_name(Type),
 	mnesia:dirty_delete(Table, Id).
 
+delete_node(Node) ->
+	case get_node_type(Node) of
+		{ok, Type} ->
+			do_delete_node(Type, Node);
+		_ ->
+			ok
+	end.
+	
+do_delete_node(Type, Node) ->				
+	mnesia:dirty_delete_object({ens_node, Type, Node}), 
+	Table = table_name(Type),
+	Record = {Table, '$1', Node, '_'},
+	case catch mnesia:dirty_select(Table, [{Record, [], ['$1']}]) of
+		[_] = Ids ->
+			[mnesia:dirty_delete(Table, Id) || Id <- Ids];
+		Reason ->
+			{error, Reason}
+	end.
+
 create(Type) ->
 	{_, Node} = get_node(Type),
 	rpc:call(Node, poolboy, checkout, [pool]).
 
 get_node(NodeType) ->
-	Nodes = mnesia:read(ens_nodes, NodeType),
+	Nodes = mnesia:read(ens_node, NodeType),
 	N = random:uniform(length(Nodes)),
 	lists:nth(N, Nodes).
+
+register_to(Node) ->
+	gen_server:cast({ens_server, Node}, {nodeup, node()}).
 
 table_name(Type) ->
 	list_to_atom(?PREFIX ++ atom_to_list(Type)).
